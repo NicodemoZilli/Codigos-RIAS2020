aef3d588f4163a091ed44ddf67371f0c3dd3c409
Nicodemo Zilli V <nicozv13@gmail.com>
2020-11-05 19:28:05 -0600

    Actualizacion

--- ejem/sencha/ejSenchaAjaxPhpBD/arriba.php
+++ ejem/sencha/ejSenchaAjaxPhpBD/arriba.php
@@ -5,11 +5,11 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 			<!-- Inclusión de hojas de estilos -->
-			<link rel="stylesheet" type="text/css" href="css/estilo1.css"> 
+			<link rel="stylesheet" type="text/css" href="css/estilo1.css">
 			<!-- Inclusión de biblioteca Sencha ExtJS -->
-			<link rel="stylesheet" type="text/css" href="jslib/theme-classic/resources/theme-classic-all.css">
-			<script type="text/javascript" src="jslib/ext-all.js"></script>
-			<script type="text/javascript" src="jslib/theme-classic/theme-classic.js"></script>
+			<link rel="stylesheet" type="text/css" href="../jslib/theme-classic/resources/theme-classic-all.css">
+			<script type="text/javascript" src="../jslib/ext-all.js"></script>
+			<script type="text/javascript" src="../jslib/theme-classic/theme-classic.js"></script>
 	</head>
 	<body>
         <div id="header">
